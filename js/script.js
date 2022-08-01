aa4d5e23b5bc154525aee2341acbcc4e598406ef
Artur Judycki <artur.judycki55@gmail.com>
2022-08-01 12:49:44 +0200

    add expand answers to faq

--- js/script.js
+++ js/script.js
@@ -14,6 +14,21 @@
 //   menu.classList.toggle("navbar--none");
 // });
 
+let questions = document.querySelectorAll(".faq__question");
+
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", popUp, false);
+}
+
+function popUp() {
+  let answer = this.querySelector(".faq__question-answer");
+  let plus = this.querySelector(".faq__question-icon");
+  let xmark = this.querySelector(".faq__question-xmark");
+  answer.classList.toggle("faq__question-answer--displayBlock");
+  plus.classList.toggle("faq__question-icon--displayNone");
+  xmark.classList.toggle("faq__question-xmark--displayBlock");
+}
+
 $(".wrapper__scroller-up").on("click", function () {
   $("body, html").animate(
     {
